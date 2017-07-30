3cf549b2ef5baeae37ad7a967ff20d04861ce3a5
Otto <otarmames@gmail.com>
2017-07-30 04:03:12 +0400

    cleaning index.php

--- public/index.php
+++ public/index.php
@@ -20,13 +20,7 @@ require '../Core/Router.php';
 
 $router = new Router();
 
-$router->add('', ['controller' => 'Home', 'action' => 'index']);
-$router->add('posts', ['controller' => 'Post', 'action' => 'index']);
 $router->add('{controller}/{action}');
-//$router->add('posts/new', ['controller' => 'Post', 'action' => 'new']);
-$router->add('admin/{action}/{controller}');
 $router->add('{controller}/{id:\d+}/{action}');
-
-
 
 $router->dispatch($_SERVER['QUERY_STRING']);
